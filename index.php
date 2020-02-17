5e948f5be00d44ece698a3dc0bb55382b2a1559b
nick van oostrom <nick.v.oostrom@gmail.com>
2020-02-17 13:43:11 +0100

    alle opnieuw

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./eigen_css/mainstyle.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Doof</title>
+</head>
+
+<body>
+    <div class="row bg-light" id="navigation">
+        <div class="col- 12">
+            <?php include("./pagina's/navigation.php"); ?>
+        </div>
+    </div>
+
+    <div class="row">
+            <div class="col-12">
+                <?php include("./pagina's/content.php"); ?>
+            </div>
+
+            <div class="row">
+            <div class="col-12">
+                <?php include("./pagina's/footer.php"); ?>
+            </div>
+        </div>
+
+
+    <script src="./jquery/jquery-3.4.1.min.js"></script>
+    <script src="./popper/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ pagina's/navigation.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-light bg-light" id="nav">
+  <a class="navbar-brand">Doof</a>
+  <form class="form-inline">
+    <ul class="navbar-nav">
+      <li class="navitem">
+        <a href="./index.php?content=home" class="navlink">Home</a>
+        <a href="./index.php?content=game" class="navlink">Game</a>
+        <a href="./index.php?content=informatie" class="navlink">Informatie</a>
+        <a href="./index.php?content=form" class="navlink">Form</a>
+        <a href="./index.php?content=contact" class="navlink">Contact</a>
+        <a href="./index.php?content=faq" class="navlink">Faq</a>
+      </li>
+    </ul>
+    <input class="form-control mr-sm-2 float-right" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0 float-right" type="submit">login</button>
+  </form>
+</nav>
