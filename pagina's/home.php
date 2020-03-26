35d2e6c280fd17e5a2e8179a8eff0fb1398d36a1
nick van oostrom <nick.v.oostrom@gmail.com>
2020-03-26 13:32:06 +0100

    home page carousel met links toegevoeg

--- pagina's/home.php
+++ pagina's/home.php
@@ -2,18 +2,38 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100 img" src="./img/doof_1.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5 class="h5home">home page</h5>
+    <a  class="homelink" href="index.php?content=home">ga naar de home pagina</a>
+    </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100 img" src="./img/doof_2.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5 class="h5home">informatie</h5>
+    <a class="homelink" href="index.php?content=informatie">ga naar de informatie pagina</a>
+    </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100 img" src="./img/contact.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5 class="h5home">contact</h5>
+    <a class="homelink" href="index.php?content=contact">ga naar de contact pagina</a>
+    </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100 img" src="./img/game.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5 class="h5home">game</h5>
+    <a class="homelink" href="index.php?content=game">ga naar de game pagina</a>
+    </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100 img" src="./img/faq.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5 class="h5home">faq</h5>
+    <a class="homelink" href="index.php?content=faq">ga naar de faq pagina</a>
+    </div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
