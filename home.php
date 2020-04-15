9d824fde3cfe07790fd4eaf5484e6f0e14970e56
tyrapeut <56019575+tyrapeut@users.noreply.github.com>
2020-04-15 13:29:02 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,44 @@
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <a href="index.php?content=informatie"> <img class="d-block w-100 img" src="./img/info_doof.png" alt="Second slide"></a>
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <a href="index.php?content=contact"> <img class="d-block w-100 img" src="./img/contact.jpg" alt="Third slide"> </a>
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <a href="index.php?content=faq"> <img class="d-block w-100 img" src="./img/faq.jpg" alt="Third slide"></a>
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+</div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="jumbotron">
+      <div class="col-6" id="Text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+      Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia.</div>
+      <div class="col-6" id="imgDoof">
+        <img src="./img/doof.jpg">
+      </div>      
+    </div>
+  </div>
+</div>
